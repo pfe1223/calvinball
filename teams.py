24e6a76a6cf542ddd25dc2cc055d4ffab1a0cb84
Patrick Ester <patricke@codio.com>
2021-05-22 18:39:20 +0000

    initial commit

---
+++ teams.py
@@ -0,0 +1,17 @@
+mlb = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'DET', 'HOU', 'KCR', 'LAA', 'LAD', 'MIA', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK', 'PHI', 'PIT', 'SDP', 'SFG', 'SEA', 'STL', 'TBR', 'TEX', 'TOR', 'WSN']
+
+al = ['BAL', 'BOS', 'CHW', 'CLE', 'DET', 'HOU', 'KCR', 'LAA', 'MIN', 'NYY', 'OAK', 'SEA', 'TBR', 'TEX', 'TOR']
+
+al_east = ['BAL', 'BOS', 'NYY', 'TBR', 'TOR']
+
+al_central = ['CHW', 'CLE', 'DET', 'KCR', 'MIN']
+
+al_west = ['HOU', 'LAA', 'OAK', 'SEA', 'TEX']
+
+nl = ['ARI', 'ATL', 'CHC', 'CIN', 'COL', 'LAD', 'MIA', 'MIL', 'NYM', 'PHI', 'PIT', 'SDP', 'SFG', 'STL', 'WSN']
+
+nl_east = ['ATL', 'MIA', 'MIL', 'NYM', 'PHI', 'WSN']
+
+nl_central = ['CHC', 'CIN', 'MIL', 'PIT', 'STL']
+
+nl_west = ['ARI', 'COL', 'LAD', 'SDP', 'SFG']
